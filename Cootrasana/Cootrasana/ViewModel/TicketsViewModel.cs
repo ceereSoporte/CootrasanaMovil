ab3113070e5e5e9c3e05746e9b941eb1f265472f
ceereSoporte <44092617+ceereSoporte@users.noreply.github.com>
2019-06-25 17:19:36 -0500

    RelayCommand

--- Cootrasana/Cootrasana/ViewModel/TicketsViewModel.cs
+++ Cootrasana/Cootrasana/ViewModel/TicketsViewModel.cs
@@ -1,7 +1,28 @@
-﻿namespace Cootrasana.ViewModel
+﻿using System.Windows.Input;
+
+namespace Cootrasana.ViewModel
 {
+
     public class TicketsViewModel : BaseViewModel
     {
 
+        public int ValTicket { get; set; }
+
+        public int NoPersonas { get; set; }
+
+
+
+        public ICommand PrintCommand
+        {
+            get
+            {
+                return new RelayCommand(Print);
+            }
+        }
+
+        private async void Print()
+        {
+            await App.Current.MainPage.DisplayAlert("Test", "Test", "OK");
+        }
     }
 }
