d8b80a968cdf693e163dab6fb978baabcfee5bb4
ceereSoporte <soporte2ceere@gmail.com>
2019-06-26 13:25:28 +0000

    Alert Ticket

--- Cootrasana/Cootrasana/ViewModel/TicketsViewModel.cs
+++ Cootrasana/Cootrasana/ViewModel/TicketsViewModel.cs
@@ -1,16 +1,29 @@
-﻿using System.Windows.Input;
+﻿
 
 namespace Cootrasana.ViewModel
 {
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
 
     public class TicketsViewModel : BaseViewModel
     {
+        #region Attributes
+        public bool isEnable;
+        #endregion
+
+        #region Properties
 
         public int ValTicket { get; set; }
 
         public int NoPersonas { get; set; }
 
-
+        public bool IsEnable
+        {
+            get { return this.isEnable; }
+            set { this.SetValue(ref this.isEnable, value); }
+        }
+    
+        #endregion
 
         public ICommand PrintCommand
         {
